e629e8c52ccb8b7792c1035319af3b6591ddabc4
devferx <ferkki@hotmail.com>
2021-01-01 10:19:58 -0400

    Clase Analizando el bundle de la aplicación

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,16 @@
 const webpack = require('webpack')
 const path = require('path')
-
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+  .BundleAnalyzerPlugin
 const nodeEnv = process.env.NODE_ENV || 'development'
+
+const shouldAnalize = process.argv.includes('--analyze')
+
+const plugins = []
+
+if (shouldAnalize) {
+  plugins.push(new BundleAnalyzerPlugin())
+}
 
 const config = {
   mode: nodeEnv,
@@ -14,6 +23,7 @@ const config = {
   devServer: {
     contentBase: '.',
   },
+  plugins,
 }
 
 module.exports = config
