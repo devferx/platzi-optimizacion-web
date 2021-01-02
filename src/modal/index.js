746cc6667f55d9d1f4153fe5c0e7f9725472511b
devferx <ferkki@hotmail.com>
2021-01-02 09:48:44 -0400

    Clase Lazy loading del modal

--- src/modal/index.js
+++ src/modal/index.js
@@ -1,10 +1,14 @@
-import { openModal } from './open-modal'
-
 export const modalListener = event => {
   event.preventDefault()
   const img = event.target
   const link = img.parentElement
+  const videoId = link.dataset.videoId
+
   if (link && link.classList.contains('js-video-link')) {
-    openModal(link.dataset.videoId)
+    import(/* webpackChunkName: "modal" */ './open-modal').then(
+      ({ openModal }) => {
+        openModal(videoId)
+      }
+    )
   }
 }
